7103ab09254040d610d2d92e26083c51e1efe2c1
eliyahoo <refaeliyahoo@gmail.com>
2021-08-08 18:23:07 +0300

    my first froject

---
+++ lesson1.py
@@ -0,0 +1,18 @@
+num=4567
+ahadot=num-4560
+asarot=ahadot-1
+meot=asarot-1
+alafim=meot-1
+
+
+
+print("ahadot " + str(ahadot))
+print("asarot: " + str(asarot))
+print("meot: " + str(meot))
+print("alafim: " + str(alafim))
+"""
+alafim=4
+meot=5
+asarot=6
+ahadot=7
+"""
